b5e8d9a9dfe87bd55dd009e064367f4f01728092
riya <riya@samyotech.com>
2025-02-25 15:56:26 +0530

    change dashboard menu

--- src/menu-items/dashboard.js
+++ src/menu-items/dashboard.js
@@ -1,7 +1,7 @@
 // assets
 import {
   IconHome,
-  IconCalendarEvent,
+  IconSettingsAutomation,
   IconMail,
   IconFileUpload,
   IconFileInvoice,
@@ -10,13 +10,13 @@ import {
   IconChecklist,
   IconNotebook,
   IconPhoneCheck,
-  IconUsers
+  IconUser
 } from '@tabler/icons';
 
 // constant
 const icons = {
   IconHome,
-  IconCalendarEvent,
+  IconSettingsAutomation,
   IconMail,
   IconFileUpload,
   IconFileInvoice,
@@ -25,13 +25,12 @@ const icons = {
   IconChecklist,
   IconNotebook,
   IconPhoneCheck,
-  IconUsers
+  IconUser
 };
 
 // ==============================|| DASHBOARD MENU ITEMS ||============================== //
 
 const dashboard = {
-  title: 'Dashboard-Menu',
   type: 'group',
   children: [
     {
@@ -43,85 +42,106 @@ const dashboard = {
       breadcrumbs: false
     },
     {
-      id: '01',
-      title: 'Lead Management',
-      type: 'item',
-      url: '/dashboard/lead',
-      icon: icons.IconAntennaBars5,
-      breadcrumbs: false
+      id: 'people',
+      title: 'People',
+      type: 'collapse', 
+      icon: icons.IconUser,
+      children: [
+        {
+          id: 'all-service-users',
+          title: 'All Service Users',
+          type: 'item',
+          url: '/dashboard/lead',
+          breadcrumbs: false
+        },
+        {
+          id: 'mailing-list',
+          title: 'Mailing List',
+          type: 'item',
+          url: '/dashboard/lead',
+          breadcrumbs: false
+        },
+        {
+          id: 'new-referral',
+          title: 'New Referral',
+          type: 'item',
+          url: '/dashboard/lead',
+          breadcrumbs: false
+        }
+      ]
     },
     {
       id: '02',
-      title: 'Contact Management',
+      title: 'Services',
       type: 'item',
       url: '/dashboard/contact',
-      icon: icons.IconPhoneCheck,
+      icon: icons.IconSettingsAutomation,
       breadcrumbs: false
     },
     {
       id: '03',
-      title: 'Policy Management',
+      title: 'Cases',
       type: 'item',
       url: '/dashboard/policy',
-      icon: icons.IconNotebook,
-      breadcrumbs: false
-    },
-    {
-      id: '04',
-      title: 'Tasks',
-      type: 'item',
-      url: '/dashboard/task',
-      icon: icons.IconChecklist,
-      breadcrumbs: false
-    },
-    {
-      id: '05',
-      title: 'Meeting',
-      type: 'item',
-      url: '/dashboard/meeting',
-      icon: icons.IconUsers,
-      breadcrumbs: false
-    },
-    {
-      id: '06',
-      title: 'Calls',
-      type: 'item',
-      url: '/dashboard/call',
-      icon: icons.IconPhoneCall,
-      breadcrumbs: false
-    },
-    {
-      id: '07',
-      title: 'Emails',
-      type: 'item',
-      url: '/dashboard/email',
-      icon: icons.IconMail,
-      breadcrumbs: false
-    },
-    {
-      id: '08',
-      title: 'Calender',
-      type: 'item',
-      url: '/dashboard/calender',
-      icon: icons.IconCalendarEvent,
-      breadcrumbs: false
-    },
-    {
-      id: '09',
-      title: 'Document Management',
-      type: 'item',
-      url: '/dashboard/document',
-      icon: icons.IconFileUpload,
-      breadcrumbs: false
-    },
-    {
-      id: '10',
-      title: 'Email Template',
-      type: 'item',
-      url: '/dashboard/emailtemplate',
       icon: icons.IconFileInvoice,
       breadcrumbs: false
-    }
+    },
+    // {
+    //   id: '04',
+    //   title: 'Tasks',
+    //   type: 'item',
+    //   url: '/dashboard/task',
+    //   icon: icons.IconChecklist,
+    //   breadcrumbs: false
+    // },
+    // {
+    //   id: '05',
+    //   title: 'Meeting',
+    //   type: 'item',
+    //   url: '/dashboard/meeting',
+    //   icon: icons.IconUsers,
+    //   breadcrumbs: false
+    // },
+    // {
+    //   id: '06',
+    //   title: 'Calls',
+    //   type: 'item',
+    //   url: '/dashboard/call',
+    //   icon: icons.IconPhoneCall,
+    //   breadcrumbs: false
+    // },
+    // {
+    //   id: '07',
+    //   title: 'Emails',
+    //   type: 'item',
+    //   url: '/dashboard/email',
+    //   icon: icons.IconMail,
+    //   breadcrumbs: false
+    // },
+    // {
+    //   id: '08',
+    //   title: 'Calender',
+    //   type: 'item',
+    //   url: '/dashboard/calender',
+    //   icon: icons.IconCalendarEvent,
+    //   breadcrumbs: false
+    // },
+    // {
+    //   id: '09',
+    //   title: 'Document Management',
+    //   type: 'item',
+    //   url: '/dashboard/document',
+    //   icon: icons.IconFileUpload,
+    //   breadcrumbs: false
+    // },
+    // {
+    //   id: '10',
+    //   title: 'Email Template',
+    //   type: 'item',
+    //   url: '/dashboard/emailtemplate',
+    //   icon: icons.IconFileInvoice,
+    //   breadcrumbs: false
+    // }
   ]
 };
 

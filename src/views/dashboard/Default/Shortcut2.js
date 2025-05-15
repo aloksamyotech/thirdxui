b2d5c2960f34757a6da80731c7f24634bb0858b2
riya <riya.rajput@samyotech.in>
2025-05-15 11:44:09 +0530

    make dashboard 1st 2 card responsive

--- src/views/dashboard/Default/Shortcut2.js
+++ src/views/dashboard/Default/Shortcut2.js
@@ -1,24 +1,19 @@
 import { Box, Stack } from '@mui/system'
 import React from 'react'
 import { Card, Typography } from '@mui/material';
-import { Person } from '@mui/icons-material';
-import SpeakerGroupIcon from '@mui/icons-material/SpeakerGroup';
-import GroupsIcon from '@mui/icons-material/Groups';
-import BusinessIcon from '@mui/icons-material/Business';
-import PublicIcon from '@mui/icons-material/Public';
-import DynamicFormIcon from '@mui/icons-material/DynamicForm';
-import Diversity2Icon from '@mui/icons-material/Diversity2';
-import PermMediaIcon from '@mui/icons-material/PermMedia';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import AirplayIcon from '@mui/icons-material/Airplay';
+import { IconSeeding, IconClipboardData} from '@tabler/icons';
 import { useNavigate } from 'react-router';
 const Shortcut2 = ({ icon, title, path }) => {
     const navigate = useNavigate()
 
     return (
         <Card sx={{ display: 'flex', flexDirection: 'row', cursor: 'pointer', border: '1px solid #00000033', justifyContent: 'center', alignItems: 'center', py: '10px' }} onClick={() => (navigate(path))}>
-            {icon == 1 && <Person sx={{ fontSize: '30px' }} />}
-            {icon == 2 && <SpeakerGroupIcon sx={{ fontSize: '30px' }} />}
-            {icon == 3 && <GroupsIcon sx={{ fontSize: '30px' }} />}
-            {icon == 4 && <BusinessIcon sx={{ fontSize: '30px' }} />}
+            {icon == 1 && <PersonOutlineIcon sx={{ fontSize: '24px' }} />}
+            {icon == 2 && <IconClipboardData sx={{ fontSize: '30px' }} />}
+            {icon == 3 && <AirplayIcon sx={{ fontSize: '24px' }} />}
+            {icon == 4 && <IconSeeding sx={{ fontSize: '30px' }} />}
             <Typography sx={{ ml: '5px' }}>{title}</Typography>
         </Card>
     )
--- src/views/dashboard/Default/index.js
+++ src/views/dashboard/Default/index.js
@@ -21,8 +21,8 @@ import footballImg from 'assets/images/football.jpg';
 import volleyballImg from 'assets/images/volleyball.jpg';
 import groupWorkImg from 'assets/images/groupWork.jpg';
 import artsImg from 'assets/images/arts.jpg';
-import cricket from 'assets/images/cricket.webp'
-import pickleball from 'assets/images/pickleball.jpg'
+import cricket from 'assets/images/cricket.webp';
+import pickleball from 'assets/images/pickleball.jpg';
 import DashboardCard from 'ui-component/cards/DashboardCard';
 import NewDashboardCard from 'ui-component/cards/NewDashboardCard';
 import Sessions from './Sessions';
@@ -45,36 +45,38 @@ const Dashboard = () => {
     <Grid container spacing={gridSpacing}>
       <Grid item xs={12}>
         <Grid container spacing={2}>
-          <Grid item xs={3}>
-            <DashboardCard title='Active Service User' num1='234' num2='119' color="linear-gradient(135deg,rgb(255, 162, 75) 0%,rgb(255, 136, 39) 100%)" color2='#ff7c11' />
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <DashboardCard title="Active Service User" num1="145 M" num2="62" />
           </Grid>
-          <Grid item xs={3}>
-            <DashboardCard title='Open Cases' num1='145' num2='62' color="linear-gradient(135deg,rgb(255, 162, 75) 0%,rgb(255, 136, 39) 100%)" color2='#ff7c11' />
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <DashboardCard title="Open Cases" num1="145 M" num2="62" />
           </Grid>
-          <Grid item xs={3}>
-            <DashboardCard title='Sessions Deliver' num1='34' num2='19' color='linear-gradient(135deg, rgb(135, 206, 250) 0%, rgb(70, 130, 180) 100%)' color2='#4682b4' />
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <DashboardCard title="Sessions Deliver" num1="145 M" num2="62" />
           </Grid>
-          <Grid item xs={3}>
-            <DashboardCard title='Total Donation' num1='145M' num2='62' color='linear-gradient(135deg, rgb(135, 206, 250) 0%, rgb(70, 130, 180) 100%)' color2='#4682b4' />
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <DashboardCard title="Total Donation" num1="145 M" num2="62" />
           </Grid>
         </Grid>
       </Grid>
+
       <Grid item xs={12}>
-        <Grid container spacing={6} sx={{ px: '20px' }}>
-          <Grid item xs={3}>
-            <Shortcut2 icon={1} title={'Add Person'} path={'/add-serviceuser'} />
+        <Grid container spacing={3} sx={{ px: '20px' }}>
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <Shortcut2 icon={1} title="Add Person" path="/add-serviceuser" />
           </Grid>
-          <Grid item xs={3}>
-            <Shortcut2 icon={2} title={'Add New Case'} path={'/add-case'} />
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <Shortcut2 icon={2} title="Add New Case" path="/add-case" />
           </Grid>
-          <Grid item xs={3}>
-            <Shortcut2 icon={3} title={'Add Session Attendies'} path={'/add-session'} />
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <Shortcut2 icon={3} title="Add Session Attendies" path="/add-session" />
           </Grid>
-          <Grid item xs={3}>
-            <Shortcut2 icon={4} title={'Add Donor'} path={'/add-donor'} />
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <Shortcut2 icon={4} title="Add Donor" path="/add-donor" />
           </Grid>
         </Grid>
       </Grid>
+
       <Grid item container xs={12} sx={{ justifyContent: 'center' }}>
         <Divider sx={{ width: '70%', borderWidth: '1px' }} />
       </Grid>

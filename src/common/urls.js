9401de7808463eb89ca9331611ef8920d788d901
Diksha kushwah <diksha.kushwah@samyotech.in>
2025-05-15 18:57:16 +0530

    session list data

--- src/common/urls.js
+++ src/common/urls.js
@@ -49,6 +49,7 @@ export const urls = Object.freeze({
   session: {
     create: `${baseUrl}/session/addSession`,
     filter: `${baseUrl}/session/search`,
-    fetch: `${baseUrl}/session/getAllSession`
+    fetch: `${baseUrl}/session/getAllSession`,
+    getById: `${baseUrl}/session/getSessionById/:id`
   }
 });

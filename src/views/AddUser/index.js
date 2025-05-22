6e7938452e9343f1768784e9fb5bb8d2e0cfb586
Diksha kushwah <diksha.kushwah@samyotech.in>
2025-05-22 17:43:14 +0530

    configuration id in add user

--- src/views/AddUser/index.js
+++ src/views/AddUser/index.js
@@ -31,7 +31,7 @@ import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import AntSwitch from 'components/AntSwitch.js';
 import dayjs from 'dayjs';
-import { postApi, getApi } from 'common/apiClient';
+import { postApi, getApi, updateApiPatch } from 'common/apiClient';
 import { urls } from 'common/urls';
 
 const AddCaseForm = ({ onCancel }) => {
@@ -41,6 +41,9 @@ const AddCaseForm = ({ onCancel }) => {
   const [restrictAccess, setRestrictAccess] = useState(false);
   const [isLoading, setIsloading] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [contactpurpose, setContactpurpose] = useState([]);
+  const [reason, setReason] = useState([]);
+  const [contactmethod, setContactmethod] = useState([]);
   const location = useLocation();
   const userdata = location.state;
   const editdata = userdata?.[0] || null;
@@ -98,9 +101,9 @@ const AddCaseForm = ({ onCancel }) => {
       emergencytown: editdata?.emergencyContact?.town || '',
       emergencypinCode: editdata?.emergencyContact?.postcode || '',
       emergencycountry: editdata?.emergencyContact?.country || '',
-      preferredContact: editdata?.contactPreferences?.preferredMethod || '',
-      reason: editdata?.contactPreferences?.reason || '',
-      contactPurpose: editdata?.contactPreferences?.contactPurposes || '',
+      preferredContact: editdata?.contactPreferences?.preferredMethod?._id || '',
+      reason: editdata?.contactPreferences?.reason?._id || '',
+      contactPurpose: editdata?.contactPreferences?.contactPurposes?._id || '',
       confirmationDate: editdata?.contactPreferences?.dateOfConfirmation || null,
       telephone: editdata?.contactPreferences?.contactMethods?.telephone || true,
       emailConsent: editdata?.contactPreferences?.contactMethods?.email || true,
@@ -141,7 +144,22 @@ const AddCaseForm = ({ onCancel }) => {
         setCountryList(countries);
       });
   }, []);
-
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getApi(urls.configuration.fetch);
+        const filterreason = response?.data?.allConfiguration?.filter((item) => item.configurationType === 'Reason');
+        setReason(filterreason);
+        const filtercontactpurpose = response?.data?.allConfiguration?.filter((item) => item.configurationType === 'Contact Purpose');
+        setContactpurpose(filtercontactpurpose);
+        const filtercontactmethod = response?.data?.allConfiguration?.filter((item) => item.configurationType === 'Contact Types');
+        setContactmethod(filtercontactmethod);
+      } catch (error) {
+        console.error('Error fetching config:', error);
+      }
+    };
+    fetchData();
+  }, []);
   const districts = [
     { label: 'Adur and Worthing Borough', value: 'adur_worthing_borough' },
     { label: 'Adur District', value: 'adur_district' },
@@ -177,7 +195,7 @@ const AddCaseForm = ({ onCancel }) => {
     fd.append('personalInfo[title]', formData.personalInfo.title || '');
     fd.append('personalInfo[gender]', formData.personalInfo.gender || '');
     const dob = formData.personalInfo.dateOfBirth;
-    fd.append('personalInfo[dateOfBirth]', dob ? new Date(dob).toISOString() : null);
+    fd.append('personalInfo[dateOfBirth]', dob ? new Date(dob).toISOString() : '');
     fd.append('personalInfo[nickName]', formData.personalInfo.nickName || '');
     fd.append('personalInfo[ethnicity]', formData.personalInfo.ethnicity || '');
 
@@ -216,9 +234,18 @@ const AddCaseForm = ({ onCancel }) => {
     fd.append('emergencyContact[town]', formData.emergencytown || '');
     fd.append('emergencyContact[postcode]', formData.emergencypinCode || '');
 
-    fd.append('contactPreferences[preferredMethod]', formData.preferredContact || '');
-    fd.append('contactPreferences[reason]', formData.reason || '');
-    fd.append('contactPreferences[contactPurposes]', formData.contactPurpose || '');
+    if (formData.preferredContact) {
+      fd.append('contactPreferences[preferredMethod]', formData.preferredContact);
+    }
+
+    if (formData.contactPurpose) {
+      fd.append('contactPreferences[contactPurposes]', formData.contactPurpose);
+    }
+
+    if (formData.reason) {
+      fd.append('contactPreferences[reason]', formData.reason);
+    }
+
     const confirmDate = formData.confirmationDate;
     fd.append('contactPreferences[dateOfConfirmation]', confirmDate ? new Date(confirmDate).toISOString() : '');
 
@@ -236,7 +263,7 @@ const AddCaseForm = ({ onCancel }) => {
 
     try {
       if (editdata) {
-        await postApi(`${urls.serviceuser.editUser}/${editdata._id}`, fd, {
+        await updateApiPatch(`${urls.serviceuser.editUser}/${editdata._id}`, fd, {
           headers: { 'Content-Type': 'multipart/form-data' }
         });
         toast.success('User updated successfully!');
@@ -1861,12 +1888,11 @@ const AddCaseForm = ({ onCancel }) => {
                           error={!!errors.preferredContact}
                           helperText={errors.preferredContact?.message}
                         >
-                          <MenuItem value="email">Email</MenuItem>
-                          <MenuItem value="phone">Phone</MenuItem>
-                          <MenuItem value="text">Text</MenuItem>
-                          <MenuItem value="letter">Letter</MenuItem>
-                          <MenuItem value="whatsapp">WhatsApp</MenuItem>
-                          <MenuItem value="doNotContact">Do not contact</MenuItem>
+                          {contactmethod?.map((option) => (
+                            <MenuItem key={option._id} value={option._id}>
+                              {option.name.charAt(0).toUpperCase() + option.name.slice(1)}
+                            </MenuItem>
+                          ))}
                         </TextField>
                       )}
                     />
@@ -1888,8 +1914,11 @@ const AddCaseForm = ({ onCancel }) => {
                           error={!!errors.contactPurpose}
                           helperText={errors.contactPurpose?.message}
                         >
-                          <MenuItem value="newsletter">Newsletter</MenuItem>
-                          <MenuItem value="upcomingEvents">Upcoming Events</MenuItem>
+                          {contactpurpose?.map((option) => (
+                            <MenuItem key={option._id} value={option._id}>
+                              {option.name.charAt(0).toUpperCase() + option.name.slice(1)}
+                            </MenuItem>
+                          ))}
                         </TextField>
                       )}
                     />
@@ -1938,10 +1967,11 @@ const AddCaseForm = ({ onCancel }) => {
                           error={!!errors.reason}
                           helperText={errors.reason?.message}
                         >
-                          <MenuItem value="interest">Legitimate Interest</MenuItem>
-                          <MenuItem value="byRequest">By Request</MenuItem>
-                          <MenuItem value="deceased">Deceased</MenuItem>
-                          <MenuItem value="goneAway">Gone Away</MenuItem>
+                          {reason?.map((option) => (
+                            <MenuItem key={option._id} value={option._id}>
+                              {option.name.charAt(0).toUpperCase() + option.name.slice(1)}
+                            </MenuItem>
+                          ))}
                         </TextField>
                       )}
                     />

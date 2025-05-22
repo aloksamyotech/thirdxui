db35ea230aa93de86ac786b3126be514ef5c41c9
vaishnavi <vaishnavichalsey11@gmail.com>
2025-05-22 11:59:32 +0530

    updated 22.01

--- src/views/Donor/index.js
+++ src/views/Donor/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Stack, Grid, Typography, Box, Card, TextField,InputBase, IconButton, Tooltip } from '@mui/material';
+import { Stack, Grid, Typography, Box, Card, TextField, InputBase, IconButton, Tooltip } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { DataGrid, GridToolbarContainer, GridToolbarExport } from '@mui/x-data-grid';
 import AddIcon from '@mui/icons-material/Add';
@@ -105,7 +105,7 @@ const Lead = () => {
 
           <Tooltip title="Info" arrow>
             <IconButton>
-              <InfoIcon sx={{ color: '#49494c' }}  />
+              <InfoIcon sx={{ color: '#49494c' }} />
             </IconButton>
           </Tooltip>
         </Stack>
@@ -127,6 +127,7 @@ const Lead = () => {
 
       if (status) queryParams.append('status', status === 'active');
       if (name) queryParams.append('name', name);
+
       if (campaign) queryParams.append('campaigns', campaign);
 
       if (dateOpenedFilter) {
@@ -144,6 +145,7 @@ const Lead = () => {
       queryParams.append('role', 'donor');
 
       const url = `${urls.serviceuser.fetchWithPagination}?${queryParams.toString()}`;
+
       const response = await getApi(url);
 
       const allDonor = response?.data?.data || [];
@@ -215,22 +217,25 @@ const Lead = () => {
         role: 'donor'
       });
       const response = await getApi(`${urls.serviceuser.fetchWithPagination}?${queryParams.toString()}`);
+
       const allDonor = response?.data?.data || [];
       const pagination = response?.data?.meta || { total: 0 };
 
-      const nameOptions = allDonor.map(donor => {
-        if (donor.subRole === 'donar_individual') {
-          return {
-            value: `${donor.personalInfo?.firstName} ${donor.personalInfo?.lastName}`,
-            label: `${donor.personalInfo?.firstName} ${donor.personalInfo?.lastName}`
-          };
-        } else {
-          return {
-            value: donor.companyInformation?.companyName || '',
-            label: donor.companyInformation?.companyName || ''
-          };
-        }
-      }).filter(option => option.value && option.value.trim() !== '');
+      const nameOptions = allDonor
+        .map((donor) => {
+          if (donor.subRole === 'donar_individual') {
+            return {
+              value: donor?._id || '',
+              label: `${donor.personalInfo?.firstName} ${donor.personalInfo?.lastName}`
+            };
+          } else {
+            return {
+              value: donor?._id || '',
+              label: donor.companyInformation?.companyName || ''
+            };
+          }
+        })
+        .filter((option) => option.value && option.value.trim() !== '');
 
       setNameFilterOptions(nameOptions);
 
@@ -276,19 +281,19 @@ const Lead = () => {
                 Add New Donor <AddIcon fontSize="small" />
               </IconButton>
             </Tooltip>
-                      <Box
+            <Box
               sx={{
                 display: 'flex',
                 alignItems: 'center',
                 backgroundColor: '#f8f9fa',
                 borderRadius: '30px',
                 paddingLeft: '16px',
-                  border: '1px solid #e0e0e0',
+                border: '1px solid #e0e0e0',
                 width: '350px',
                 height: '40px'
               }}
             >
-            <InputBase
+              <InputBase
                 placeholder="Search..."
                 value={searchQuery}
                 onChange={handleSearchChange}
@@ -302,7 +307,7 @@ const Lead = () => {
                   color: 'text.primary'
                 }}
               />
-            <IconButton
+              <IconButton
                 onClick={handleFilter}
                 sx={{
                   marginRight: '8px',
@@ -310,9 +315,9 @@ const Lead = () => {
                   height: 32,
                   cursor: 'pointer'
                 }}
-            >
-            <SearchIcon />
-            </IconButton>
+              >
+                <SearchIcon />
+              </IconButton>
             </Box>
 
             {/* <TextField

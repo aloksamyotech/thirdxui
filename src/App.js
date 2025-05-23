958156a1bf81b25fc36487fa327d18533573d0a0
amansamyotech <aman.asati@samyotech,com>
2025-05-23 16:14:19 +0530

    check sever error page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import NavigationScroll from 'layout/NavigationScroll';
 const App = () => {
   const customization = useSelector((state) => state.customization);
 
+  
   return (
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={themes(customization)}>

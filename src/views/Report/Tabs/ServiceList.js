1de10e0fae623ef5868b5375e3547e5a24b93e59
unknown <Shubham9Gehlod>
2025-05-28 18:37:35 +0530

    Integrate the Service user report list in list view

--- src/views/Report/Tabs/ServiceList.js
+++ src/views/Report/Tabs/ServiceList.js
@@ -4,43 +4,21 @@ import { DataGrid, GridToolbarContainer, GridToolbarExport } from '@mui/x-data-g
 import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import flag from '../../../assets/images/Flag_of_India.svg';
+import { getApi } from 'common/apiClient';
+import { urls } from 'common/urls';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const ServiceList = () => {
-  const rows = [
-    {
-      id: 1,
-      userid: 'D-123',
-      name: 'John Doe',
-      dob: '27-03-04',
-      age: '20',
-      country: 'India',
-      gender: 'Male',
-      ethicity: 'Black',
-      no: '9875646643'
-    },
-    {
-      id: 2,
-      userid: 'D-123',
-      name: 'John Doe',
-      dob: '27-03-04',
-      age: '20',
-      country: 'India',
-      gender: 'Male',
-      ethicity: 'Black',
-      no: '9875646643'
-    },
-    {
-      id: 3,
-      userid: 'D-123',
-      name: 'Snow',
-      dob: '27-03-04',
-      age: '20',
-      country: 'India',
-      gender: 'Male',
-      ethicity: 'Black',
-      no: '9875646643'
-    }
-  ];
+  const [paginationModel, setPaginationModel] = useState({
+    page: 0,
+    pageSize: 10
+  });
+  const [loading, setLoading] = useState(true);
+  const [includeArchives, setIncludeArchives] = useState(false);
+  const [rows, setRows] = useState([]);
+  const [countriesWithFlags, setCountriesWithFlags] = useState([]);
+  const [totalRows, setTotalRows] = useState(0);
 
   const columns = [
     {
@@ -53,7 +31,12 @@ const ServiceList = () => {
       field: 'name',
       headerName: 'Name',
       flex: 1.5,
-      renderCell: (params) => <Typography>{params?.value}</Typography>
+      renderCell: (params) => (
+        <Typography>
+          {params.row.firstName} {params.row.lastName}
+          {params?.value}
+        </Typography>
+      )
     },
     {
       field: 'dob',
@@ -71,7 +54,6 @@ const ServiceList = () => {
             width: 35,
             height: 30,
             borderRadius: '40%',
-            backgroundColor: '#f0f0f0',
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
@@ -91,8 +73,10 @@ const ServiceList = () => {
       flex: 1.5,
       renderCell: (params) => (
         <Stack direction="row">
-          <img src={flag} alt="flag" style={{ width: 20, height: 20, objectFit: 'contain' }} />
-          <Typography sx={{ ml: '5px' }}>{params?.value}</Typography>
+          {params.row.countryFlag && (
+            <img src={params.row.countryFlag} alt={params.row.country} style={{ width: 20, height: 20, objectFit: 'contain' }} />
+          )}
+          <Typography sx={{ ml: '5px' }}>{params.row.country}</Typography>
         </Stack>
       )
     },
@@ -115,7 +99,18 @@ const ServiceList = () => {
       renderCell: (params) => <Typography>{params?.value}</Typography>
     }
   ];
-
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((res) => res.json())
+      .then((data) => {
+        const countries = data.map((country) => ({
+          value: country.cca2,
+          label: country.name.common,
+          flag: country.flags.png
+        }));
+        setCountriesWithFlags(countries);
+      });
+  }, []);
   const CustomHeader = () => {
     return (
       <Box sx={{ height: '50px', display: 'flex', alignItems: 'center' }}>
@@ -156,14 +151,91 @@ const ServiceList = () => {
       </Box>
     );
   };
+  const getAge = (dob) => {
+    const birthDate = new Date(dob);
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  };
+
+  const fetchpeople = async () => {
+    try {
+      setLoading(true);
+      const queryParams = new URLSearchParams({
+        page: paginationModel.page + 1,
+        limit: paginationModel.pageSize,
+        role: 'service_user'
+      });
+
+      if (!includeArchives) {
+        queryParams.append('archive', 'false');
+      }
+
+      const response = await getApi(`${urls.serviceuser.fetchWithPagination}?${queryParams.toString()}`);
+      const allUser = response?.data?.data || [];
+      const pagination = response?.data?.meta || { total: 0 };
+
+      const formattedUsers = allUser?.map((user, index) => {
+        const dob = user.personalInfo?.dateOfBirth ? new Date(user.personalInfo?.dateOfBirth).toLocaleDateString() : '-';
+        const age = dob !== '-' ? getAge(dob) : '-';
+        const countryName = user?.contactInfo?.country || '-';
+        const matchedCountry = countriesWithFlags.find((c) => c.label.toLowerCase() === countryName.toLowerCase());
+
+        return {
+          id: user._id,
+          serialNumber: `#C-${(index + 1).toString().padStart(3, '0')}`,
+          userid: '-',
+          firstName: user.personalInfo?.firstName || '-',
+          lastName: user.personalInfo?.lastName || '-',
+          dob,
+          age,
+          country: countryName,
+          countryFlag: matchedCountry?.flag || '',
+          ethicity: user.personalInfo?.ethnicity || '-',
+          no: user.contactInfo?.phone || '-',
+          gender: user.personalInfo?.gender || ''
+        };
+      });
+
+      setRows(formattedUsers);
+      setTotalRows(pagination?.total);
+    } catch (error) {
+      console.error('Failed to fetch services:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchpeople();
+  }, [paginationModel]);
 
   return (
     <>
       <Grid container>
-        <Box  sx={{ backgroundColor: '#fff', borderRadius: 2 }} height="auto" width="100%">
+        <Box sx={{ backgroundColor: '#fff', borderRadius: 2 }} height="auto" width="100%">
           <DataGrid
-            rows={rows ? rows : []}
+            rows={
+              loading
+                ? []
+                : rows.map((row, index) => ({
+                    ...row,
+                    sNo: paginationModel.page * paginationModel.pageSize + index + 1
+                  }))
+            }
             columns={columns}
+            loading={loading}
+            pagination
+            paginationMode="server"
+            paginationModel={paginationModel}
+            onPaginationModelChange={setPaginationModel}
+            pageSizeOptions={[10]}
+            rowCount={totalRows}
+            rowHeight={65}
             getRowId={(rows) => rows?.id}
             checkboxSelection
             components={{

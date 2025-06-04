5f04617cda0eb04a77bf6dbace4a0bf81690a947
Shubham Gehlod <Shubham9Gehlod>
2025-06-04 11:06:07 +0530

    Add scroll bar in configuration

--- src/views/Configuration/index.js
+++ src/views/Configuration/index.js
@@ -497,10 +497,8 @@ const TabbedDataGrid = () => {
                         p: 0,
                         border: 'none',
                         borderRadius: '0',
-                        height: 'auto',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        overflow: 'hidden'
+                        height: '200px',
+                        overflowY: 'auto'
                       }}
                     >
                       {cardBodyContent(
